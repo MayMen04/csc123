73f94b60347312c225e8d4e49124fb9df641234e
Maynor <maynormendieta84@gmail.com>
2023-10-23 19:11:53 -0700

    Assignment 3 Commit Attempt 2

---
+++ csc123/assn3/ShoppingCart.java
@@ -0,0 +1,60 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class ShoppingCart {
+	String name;
+	String vendor = "N/A";
+	String author;
+	String pMonth;
+	int pageNum;
+	String sellby;
+	String useby;
+	double price;
+	double cost;
+	double weight;
+	final double taxRate = 1.0725;
+	final double tax = 7.25;
+	double total;
+	public ShoppingCart(String name, String vendor, double price, double cost, double weight) {
+		this.name = name;
+		this.vendor = vendor;
+		this.price = price;
+		this.cost = cost;
+		this.weight = weight;
+		total = this.price * taxRate;
+		total = Math.floor(total*100)/100;
+	}
+	public ShoppingCart(String name, String vendor, String author, String pMonth, int pageNum, double price, double cost, double weight) {
+		this.name = name;
+		this.vendor = vendor;
+		this.author = author;
+		this.pMonth = pMonth;
+		this.pageNum = pageNum;
+		this.price = price;
+		this.cost = cost;
+		this.weight = weight;
+		total = this.price * taxRate;
+		total = Math.round(total*100)/100;
+		
+	}
+	public ShoppingCart(String name, String vendor, Date sellBy, Date useBy, double price, double cost, double weight) {
+		this.name = name;
+		this.vendor = vendor;
+		this.price = price;
+		this.cost = cost;
+		this.weight = weight;
+		SimpleDateFormat df = new SimpleDateFormat("MM/dd/yy");
+		this.sellby = df.format(sellBy);
+		this.useby = df.format(useBy);
+		total = this.price * taxRate;
+		total = Math.round(total*100)/100;
+	}
+	double getTotal() {
+		return total;
+	}
+	@Override
+	public String toString() {
+		return "Name: " + name + " - Price: " + price + "$ - Tax: " + tax + "% - Total item price: " + total + "$";
+	}
+}
+
---
+++ csc123/assn3/ShoppingCartManager.java
@@ -0,0 +1,99 @@
+import java.text.*;
+import java.util.*;
+public class ShoppingCartManager {
+	static ArrayList<ShoppingCart> itemList = new ArrayList<ShoppingCart>();
+	public static void main(String[] args) throws ParseException {
+		menu();
+	}
+	static void menu() throws ParseException {
+		Scanner input = new Scanner(System.in);
+		SimpleDateFormat df = new SimpleDateFormat("MM/dd/yyyy");
+		int amount = 0;
+		String name;
+		String vendor;
+		String type;
+		double price;
+		double cost;
+		double weight;
+		
+		String author;
+		String pMonth;
+		int pageNum;
+		
+		Date sellBy = null;
+		Date useBy = null;
+		
+		int choice = 0;
+		do {
+			System.out.print("1.) Add item\n2.) View items\n3.) Exit\nEnter choice: ");
+			if(input.hasNextInt()) {
+				choice = input.nextInt();
+				if(choice > 0 && choice < 4) 
+					break;
+			}
+			System.out.println("\nEnter a valid choice\n");
+			input.nextLine();
+		}while(true);
+		switch(choice) {
+		case 1:
+			System.out.print("\nName: ");
+			name = input.next();
+			System.out.print("Vendor: ");
+			vendor = input.next();
+			System.out.print("Price: ");
+			price = input.nextDouble();
+			System.out.print("Cost: ");
+			cost = input.nextDouble();
+			System.out.print("Weight (lbs): ");
+			weight = input.nextDouble();
+			int newChoice = 0;
+			do {
+				System.out.print("\n1.) Publication\n2.) Food\n3.) General\nEnter choice: ");
+				if(input.hasNextInt()) {
+					newChoice = input.nextInt();
+					if(newChoice > 0 && newChoice < 4) 
+						break;
+				}
+				System.out.println("\nEnter a valid choice\n");
+				input.nextLine();
+			}while(true);
+			switch(newChoice) {
+			case 1: 
+				System.out.print("Author: ");
+				author = input.next();
+				System.out.print("Publication month: ");
+				pMonth = input.next();
+				System.out.print("Number of pages: ");
+				pageNum = input.nextInt();
+				itemList.add(new ShoppingCart(name, vendor, author, pMonth, pageNum, price, cost, weight));
+				break;
+			case 2: 
+				System.out.println("Sell by (mm/dd/yyyy): ");
+				sellBy = df.parse(input.next());
+				System.out.println("Use by (mm/dd/yyyy): ");
+				useBy = df.parse(input.next());
+				itemList.add(new ShoppingCart(name, vendor, sellBy, useBy, price, cost, weight));
+				break;
+			case 3:
+				itemList.add(new ShoppingCart(name, vendor, price, cost, weight));
+				break;
+			}
+			amount++;
+			System.out.println();
+			menu();
+			break;
+		case 2: 
+			double total = 0;
+			System.out.println("\nAmount of items: " +amount);
+			for(int i = 0; i < itemList.size(); i++) {
+				System.out.println(itemList.get(i));
+				total += itemList.get(i).getTotal();
+			}
+			total = Math.floor(total * 100) / 100;
+			System.out.println("Total: "+total+"$\n");
+			menu();
+			break;
+		case 3: break;
+		}
+	}
+}
